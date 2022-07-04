2e8c8359dfdd942a166d1daf2ed8b8686f06f16c
Elf <Elf@Elf.local>
2022-07-04 11:02:27 +0400

    fix: change link in appLoader for deploy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'e419d1ff1ce44083bfd955856da5afb6',
     });
   }
